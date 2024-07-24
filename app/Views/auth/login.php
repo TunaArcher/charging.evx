ecbfccdc47fce7feae744092dbf4272a16c675b0
TunaArcher <9tnk.sl@gmail.com>
2024-07-24 16:14:57 +0700

    update

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -20,10 +20,58 @@
     * {
       font-family: 'Kanit', sans-serif;
     }
+
+    .bg-primary {
+      background-color: #01c0c8 !important;
+      color: #000 !important;
+    }
+
+    /* 
     .auth-bg {
       background: url('https://cdn.whichcar.com.au/assets/p_4x3/13f31229/evx-kerbside-charger-nsw-3.png');
       background-size: cover;
       background-repeat: no-repeat;
+    } */
+
+    .bg-svg {
+      position: relative;
+      overflow-x: hidden;
+    }
+
+    .bg-svg::before {
+      content: "";
+      position: absolute;
+      background: url(https://cdn.whichcar.com.au/assets/p_4x3/13f31229/evx-kerbside-charger-nsw-3.png);
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      width: 100%;
+      height: 100%;
+      opacity: 0.35;
+    }
+
+    .z-index-10 {
+      z-index: 10;
+    }
+
+    .page {
+      overflow: clip;
+    }
+
+    .page {
+      display: flex;
+      flex-direction: column;
+      min-height: 100vh;
+      justify-content: center;
+      border-radius: 0;
+      box-shadow: 0 0 5px var(--primary-bg-color);
+      background-position: center 64px;
+      background-repeat: no-repeat;
+      content: "";
+      inset-inline-start: 0;
+      inset-inline-end: 0;
+      top: 0;
+      width: 100%;
     }
   </style>
   <script>
@@ -31,93 +79,100 @@
   </script>
 </head>
 
-<body>
+<body class="bg-primary">
   <!-- Preloader -->
   <div class="preloader">
     <img src="<?php echo base_url('assets/images/logos/logo.png'); ?>" alt="loader" class="lds-ripple img-fluid" />
   </div>
-  <div id="main-wrapper">
-    <div class="position-relative overflow-hidden auth-bg min-vh-100 w-100 d-flex align-items-center justify-content-center">
-      <div class="d-flex align-items-center justify-content-center w-100">
-        <div class="row justify-content-center w-100 my-5 my-xl-0">
-          <div class="col-md-6 d-flex flex-column justify-content-center">
-            <div class="card mb-0 bg-body auth-login m-auto w-100">
-              <div class="row gx-0">
-                <!-- ------------------------------------------------- -->
-                <!-- Part 1 -->
-                <!-- ------------------------------------------------- -->
-                <div class="col-xl-12 border-end">
-                  <div class="row justify-content-center py-4">
-                    <div class="col-lg-11">
-                      <div class="card-body">
-                        <a href="<?php echo base_url(); ?>" class="text-nowrap logo-img d-block mb-4 w-100">
-                          <img src="<?php echo base_url('assets/images/logos/logo.png'); ?>" class="dark-logo" alt="Logo-Dark" />
-                        </a>
-                        <h2 class="lh-base mb-4">Let's get you signed in</h2>
-                        <div class="row">
-                          <div class="col-6 mb-2 mb-sm-0">
-                            <a class="disabled btn btn-white shadow-sm text-dark link-primary border fw-semibold d-flex align-items-center justify-content-center rounded-1 py-6" href="javascript:void(0)" role="button">
-                              <img src="../assets/images/svgs/facebook-icon.svg" alt="matdash-img" class="img-fluid me-2" width="18" height="18">
-                              <span class="d-none d-xxl-inline-flex"> Sign in with </span>&nbsp; Facebook
-                            </a>
-                          </div>
-                          <div class="col-6">
-                            <a class="disabled btn btn-white shadow-sm text-dark link-primary border fw-semibold d-flex align-items-center justify-content-center rounded-1 py-6" href="javascript:void(0)" role="button">
-                              <img src="../assets/images/svgs/google-icon.svg" alt="matdash-img" class="img-fluid me-2" width="18" height="18">
-                              <span class="d-none d-xxl-inline-flex"> Sign in with </span>&nbsp; Google
-                            </a>
+  <div class="bg-svg">
+    <div class="page">
+      <div class="z-index-10">
+        <div id="main-wrapper">
+          <div class="position-relative overflow-hidden min-vh-100 w-100 d-flex align-items-center justify-content-center">
+            <div class="d-flex align-items-center justify-content-center w-100">
+              <div class="row justify-content-center w-100 my-5 my-xl-0">
+                <div class="col-md-6 d-flex flex-column justify-content-center">
+                  <div class="card mb-0 bg-body auth-login m-auto w-100">
+                    <div class="row gx-0">
+                      <!-- ------------------------------------------------- -->
+                      <!-- Part 1 -->
+                      <!-- ------------------------------------------------- -->
+                      <div class="col-xl-12 border-end">
+                        <div class="row justify-content-center py-4">
+                          <div class="col-lg-11">
+                            <div class="card-body">
+                              <a href="<?php echo base_url(); ?>" class="text-nowrap logo-img d-block mb-4 w-100">
+                                <img src="<?php echo base_url('assets/images/logos/logo.png'); ?>" class="dark-logo" alt="Logo-Dark" />
+                              </a>
+                              <h2 class="lh-base mb-4">Let's get you signed in</h2>
+                              <div class="row">
+                                <div class="col-6 mb-2 mb-sm-0">
+                                  <a class="disabled btn btn-white shadow-sm text-dark link-primary border fw-semibold d-flex align-items-center justify-content-center rounded-1 py-6" href="javascript:void(0)" role="button">
+                                    <img src="../assets/images/svgs/facebook-icon.svg" alt="matdash-img" class="img-fluid me-2" width="18" height="18">
+                                    <span class="d-none d-xxl-inline-flex"> Sign in with </span>&nbsp; Facebook
+                                  </a>
+                                </div>
+                                <div class="col-6">
+                                  <a class="disabled btn btn-white shadow-sm text-dark link-primary border fw-semibold d-flex align-items-center justify-content-center rounded-1 py-6" href="javascript:void(0)" role="button">
+                                    <img src="../assets/images/svgs/google-icon.svg" alt="matdash-img" class="img-fluid me-2" width="18" height="18">
+                                    <span class="d-none d-xxl-inline-flex"> Sign in with </span>&nbsp; Google
+                                  </a>
 
-                          </div>
-                        </div>
-                        <div class="position-relative text-center my-4">
-                          <p class="mb-0 fs-12 px-3 d-inline-block bg-body z-index-5 position-relative">Or sign in with
-                            email
-                          </p>
-                          <span class="border-top w-100 position-absolute top-50 start-50 translate-middle"></span>
-                        </div>
-                        <form id="loginForm" method="post">
-                          <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email Address</label>
-                            <input name="username" type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your email" aria-describedby="emailHelp">
-                          </div>
-                          <div class="mb-4">
-                            <div class="d-flex align-items-center justify-content-between">
-                              <label for="exampleInputPassword1" class="form-label">Password</label>
-                              <a class="text-primary link-dark fs-2" href="">Forgot
-                                Password ?</a>
-                            </div>
-                            <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
-                          </div>
-                          <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                              <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>
-                              <label class="form-check-label text-dark" for="flexCheckChecked">
-                                Keep me logged in
-                              </label>
-                            </div>
-                          </div>
-                          <button id="btn-login" class="btn btn-dark w-100 py-8 mb-4 rounded-1">Sign In</button>
-                          <!-- <div class="d-flex align-items-center">
+                                </div>
+                              </div>
+                              <div class="position-relative text-center my-4">
+                                <p class="mb-0 fs-12 px-3 d-inline-block bg-body z-index-5 position-relative">Or sign in with
+                                  email
+                                </p>
+                                <span class="border-top w-100 position-absolute top-50 start-50 translate-middle"></span>
+                              </div>
+                              <form id="loginForm" method="post">
+                                <div class="mb-3">
+                                  <label for="exampleInputEmail1" class="form-label">Email Address</label>
+                                  <input name="username" type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your email" aria-describedby="emailHelp">
+                                </div>
+                                <div class="mb-4">
+                                  <div class="d-flex align-items-center justify-content-between">
+                                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                                    <a class="text-primary link-dark fs-2" href="">Forgot
+                                      Password ?</a>
+                                  </div>
+                                  <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
+                                </div>
+                                <div class="d-flex align-items-center justify-content-between mb-4">
+                                  <div class="form-check">
+                                    <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>
+                                    <label class="form-check-label text-dark" for="flexCheckChecked">
+                                      Keep me logged in
+                                    </label>
+                                  </div>
+                                </div>
+                                <button id="btn-login" class="btn btn-dark w-100 py-8 mb-4 rounded-1">Sign In</button>
+                                <!-- <div class="d-flex align-items-center">
                             <p class="fs-12 mb-0 fw-medium">Don’t have an account yet?</p>
                             <a class="text-primary fw-semibold ms-2" href="<?php echo base_url('register'); ?>">Sign Up Now</a>
                           </div> -->
-                        </form>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+
                       </div>
+                      <!-- ------------------------------------------------- -->
+                      <!-- Part 2 -->
+                      <!-- ------------------------------------------------- -->
                     </div>
+
                   </div>
-
                 </div>
-                <!-- ------------------------------------------------- -->
-                <!-- Part 2 -->
-                <!-- ------------------------------------------------- -->
               </div>
-
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
   <div class="dark-transparent sidebartoggler"></div>
   <!-- Import Js Files -->
   <script src="<?php echo base_url('/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js'); ?>"></script>
